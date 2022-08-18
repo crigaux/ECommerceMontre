1a2e7d5db055f5e6603a64c00d8ea599605ae68a
crigaux <crigaux057@gmail.com>
2022-08-18 15:03:58 +0200

    liste des produits terminée

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,23 @@
 // Les données qui doivent apparaître dans une carte sont : "id" (en display none), "image", "title", "desc", "price"
 // Trier les produits en fonction de leur "type" (classic, luxe, connected) pour savoir où injecter le html
 
-function createProductCard(element);
+function createProductCard(element) {
+    document.getElementById(element.type).innerHTML += 
+    `<div class="productCard">
+        <img src="${element.image}" alt="image de montre">
+        <h4>${element.title}</h4>
+        <p class="productPrice">${element.price}€</p>
+        <p class="productDesc">${element.desc}</p>
+        <button>ajouter au panier</button>
+    </div>`
+}
+
+fetch('/assets/js/stock.json')
+.then(function(response) {
+    return response.json();
+})
+.then(function(json) {
+    json.stock.forEach(element => {
+        createProductCard(element);
+    });
+});
