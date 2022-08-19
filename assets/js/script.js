3781a23bbf5b9f7f1836d8a19418bf8b13251b25
ChrysJ <86663951+ChrysJ@users.noreply.github.com>
2022-08-19 14:20:44 +0200

    on en a chié mais c'est bon

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 // Les données qui doivent apparaître dans une carte sont : "image", "title", "desc", "price"
 // Trier les produits en fonction de leur "type" (classic, luxe, connected) pour savoir où injecter le html
 
+const containerItems = document.querySelector(".containerItems");
+
+
 function createProductCard(product) {
     document.getElementById(product.type).innerHTML += 
     `<div class="productCard">
@@ -11,9 +14,28 @@ function createProductCard(product) {
         <h4>${product.title}</h4>
         <p class="productPrice">${product.price}€</p>
         <p class="productDesc">${product.desc}</p>
-        <button id="${product.id}">ajouter au panier</button>
+        <button class="btnAddProduct" id="${product.id}">ajouter au panier</button>
     </div>`
 }
+
+window.addEventListener('load', () => {
+    for(i=0 ; i < localStorage.length ; i++) {
+        let product = JSON.parse(localStorage.getItem(localStorage.key(i)))
+        containerItems.innerHTML +=
+        `
+        <div class="cartProductCard">
+            <div class="imgCart">
+                <img src="${product.image}">
+            </div>
+            <div class="productInfo">
+                <h4>${product.title}</h4>
+                <span>${product.price}€</span>
+                <span>${product.price * product.quantity}€</span>
+            </div>
+        </div>
+        `
+    }
+})
 
 fetch('/assets/js/stock.json')
 .then(function(response) {
@@ -23,4 +45,38 @@ fetch('/assets/js/stock.json')
     json.stock.forEach(element => {
         createProductCard(element);
     });
-});
+
+    const btnAddProduct = document.querySelectorAll(".btnAddProduct");
+        btnAddProduct.forEach((btn) => {
+            btn.addEventListener("click", (e) => {
+                addToCart(e, json.stock);
+                for(i=0 ; i < localStorage.length ; i++) {
+                    if(btn.id == localStorage.key(i)){
+                        product = JSON.parse(localStorage.getItem(localStorage.key(i)))
+                        containerItems.innerHTML +=
+                        `
+                        <div class="cartProductCard">
+                            <div class="imgCart">
+                                <img src="${product.image}">
+                            </div>
+                            <div class="productInfo">
+                                <h4>${product.title}</h4>
+                                <span>${product.price}€</span>
+                                <span>${product.price * product.quantity}€</span>
+                            </div>
+                        </div>
+                        `
+                    }
+                }
+            })
+        })
+    });
+    
+const addToCart = (e, stock) => {
+    stock.forEach((elem) => {
+        if (elem.id == e.target.id){
+            localStorage.setItem(elem.id, JSON.stringify(elem));
+        }
+    })
+}
+
