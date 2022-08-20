d4a4e04cc8464393ba2ed0a9902e42ddd20fc4c7
crigaux <crigaux057@gmail.com>
2022-08-20 09:40:49 +0200

    css merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,13 @@ function createProductCard(product) {
     </div>`
 }
 
+const addToCart = (e, stock) => {
+    stock.forEach((elem) => {
+        if (elem.id == e.target.id) {
+            localStorage.setItem(elem.id, JSON.stringify(elem));
+        }
+    })
+}
 
 function increaseQuantity(id) {
     let product = JSON.parse(localStorage.getItem(id));
@@ -25,39 +32,81 @@ function increaseQuantity(id) {
     localStorage.setItem(id, JSON.stringify(product));
 }
 
+function decreaseQuantity(id) {
+    let product = JSON.parse(localStorage.getItem(id));
+    if (product.quantity == 1) {
+        localStorage.removeItem(id);
+    } else {
+        product.quantity--;
+        localStorage.setItem(id, JSON.stringify(product));
+    }
+}
+
+function addOneProduct() {
+    document.querySelectorAll('.increase').forEach(btn => {
+        btn.addEventListener('click', () => {
+            increaseQuantity(btn.dataset.id);
+            let quantity = JSON.parse(localStorage.getItem(btn.dataset.id));
+            document.querySelector('.increase[data-id ="'+ btn.dataset.id +'"] + div').innerHTML = quantity.quantity;
+            document.querySelector('span[data-id ="'+ btn.dataset.id +'"]').innerHTML = quantity.price * quantity.quantity + '€';
+        })
+    })
+}
+
+function substractOneProduct() {
+    document.querySelectorAll('.decrease').forEach(btn => {
+        btn.addEventListener('click', () => {
+            decreaseQuantity(btn.dataset.id);
+            let quantity = JSON.parse(localStorage.getItem(btn.dataset.id));
+            document.querySelector('.increase[data-id ="'+ btn.dataset.id +'"] + div').innerHTML = quantity.quantity;
+            document.querySelector('span[data-id ="'+ btn.dataset.id +'"]').innerHTML = quantity.price * quantity.quantity + '€';
+        })
+    })
+}
+
+function deleteProduct() {
+    document.querySelectorAll('.delete-product').forEach(btn => {
+        btn.addEventListener('click', () => {
+            localStorage.removeItem(btn.dataset.id);
+            document.querySelector('.cartProductCard[data-id ="' + btn.dataset.id + '"]').outerHTML = '';
+        })
+    })
+}
+
 function displayCart() {
     containerItems.innerHTML = '';
     for (i = 0; i < localStorage.length; i++) {
         product = JSON.parse(localStorage.getItem(localStorage.key(i)))
         containerItems.innerHTML +=
-            `
-        <div class="cartProductCard">
+        `
+        <div class="cartProductCard" data-id="${product.id}">
             <div class="imgCart">
                 <img src="${product.image}">
             </div>
             <div class="productInfo">
                 <h4>${product.title}</h4>
                 <span>${product.price}€</span>
-                <span>${product.price * product.quantity}€</span>
+                <span data-id="${product.id}">${product.price * product.quantity}€</span>
             </div>
             <div>
-            <div class="addproduct-ctn">
-                <button><i class="fa-solid fa-minus"></i></button>
-                <div>${product.quantity}</div>
-                <button><i class="fa-solid fa-plus"></i></button>
+                <div class="addproduct-ctn">
+                    <button data-id="${product.id}" class="increase"><i class="fa-solid fa-plus"></i></button>
+                    <div>${product.quantity}</div>
+                    <button data-id="${product.id}" class="decrease"><i class="fa-solid fa-minus"></i></button>
+                </div>
             </div>
-        </div>
             <div class="ctn-delete-product">
-                <button class="delete-product"><i class="fa-solid fa-trash"></i></button>
+                <button class="delete-product" data-id="${product.id}"><i class="fa-solid fa-trash"></i></button>
             </div>
         </div>
         `
     }
 }
 
-window.addEventListener('load', () => {
-    displayCart();
-})
+displayCart();
+addOneProduct();
+substractOneProduct();
+deleteProduct();
 
 fetch('/assets/js/stock.json')
     .then(function (response) {
@@ -71,14 +120,14 @@ fetch('/assets/js/stock.json')
         const btnAddProduct = document.querySelectorAll(".btnAddProduct");
         btnAddProduct.forEach((btn) => {
             btn.addEventListener("click", (e) => {
-                if (localStorage.length == 0) {
+                
+                if(localStorage.length == 0) {
                     addToCart(e, json.stock);
                     displayCart();
                 } else {
                     for (i = 0; i < localStorage.length; i++) {
                         product = JSON.parse(localStorage.getItem(localStorage.key(i)))
                         if (btn.id == product.id) {
-                            console.log("déjà présent");
                             increaseQuantity(btn.id);
                             displayCart();
                         } else {
@@ -87,14 +136,34 @@ fetch('/assets/js/stock.json')
                         }
                     }
                 }
+                addOneProduct();
+                substractOneProduct();
+                deleteProduct();
             })
         })
     })
 
-const addToCart = (e, stock) => {
-    stock.forEach((elem) => {
-        if (elem.id == e.target.id) {
-            localStorage.setItem(elem.id, JSON.stringify(elem));
-        }
-    })
+// ouverture / fermeture de la modale panier
+
+const modale = document.querySelector(".modale-background");
+const basket = document.querySelector(".container-basket");
+const btnCloseModale = document.querySelector(".btn-close-modale")
+const backgroundModale = document.querySelector(".modale-background")
+
+navShoppingCartBtn.addEventListener("click", () => {
+    modaleOpen()
+})
+
+continueShopping.addEventListener("click", () => {
+    modaleOpen()
+})
+
+btnCloseModale.addEventListener("click", () => {
+    modaleOpen()
+})
+
+const modaleOpen = () => {
+    modale.classList.toggle("modaleActive")
+    document.body.classList.toggle("bodyActive")
+    basket.classList.toggle("containerBasketActive")
 }
